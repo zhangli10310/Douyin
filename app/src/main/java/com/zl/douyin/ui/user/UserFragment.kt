32c089b306693df52ac3179b4f2c99789c5118cf
zhangli03 <zhangli03@meicai.cn>
2018-03-20 16:02:05 +0800

    。

--- app/src/main/java/com/zl/douyin/ui/user/UserFragment.kt
+++ app/src/main/java/com/zl/douyin/ui/user/UserFragment.kt
@@ -2,11 +2,14 @@ package com.zl.douyin.ui.user
 
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.zl.core.base.ModeFragment
 import com.zl.core.utils.DisplayUtils
 import com.zl.core.view.GridSpacingItemDecoration
 import com.zl.douyin.R
 import kotlinx.android.synthetic.main.fragment_user.*
+import com.bumptech.glide.Glide
+
 
 /**
  *
@@ -24,13 +27,43 @@ class UserFragment : ModeFragment() {
         recyclerView.setHasFixedSize(true)
         recyclerView.isNestedScrollingEnabled = false
 
-
     }
 
     override fun setListener() {
         backImg.setOnClickListener {
             activity.onBackPressed()
         }
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                when (newState) {
+                    RecyclerView.SCROLL_STATE_IDLE ->
+                        //当屏幕停止滚动，加载图片
+                        try {
+                            Glide.with(activity).resumeRequests()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+
+                    RecyclerView.SCROLL_STATE_DRAGGING ->
+                        //当屏幕滚动且用户使用的触碰或手指还在屏幕上，停止加载图片
+                        try {
+                            Glide.with(activity).pauseRequests()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+
+                    RecyclerView.SCROLL_STATE_SETTLING ->
+                        //由于用户的操作，屏幕产生惯性滑动，停止加载图片
+                        try {
+                            Glide.with(activity).pauseRequests()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                }
+            }
+        })
     }
 
     override fun observe() {
