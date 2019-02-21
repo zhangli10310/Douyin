75ad76a77a1b472570e53e69651a6cef50720a91
zhangli03 <zhangli03@meicai.cn>
2019-02-21 18:34:39 +0800

    .

---
+++ module_main/src/main/java/com/zl/douyin/ui/mainpage/GlidePreLoad.kt
@@ -0,0 +1,11 @@
+package com.zl.douyin.ui.mainpage
+
+/**
+ *
+ *<p></p>
+ *
+ * Created by zhangli on 2019/2/20 14:44.<br/>
+ * Copyright (c) 2015年 Beijing Yunshan Information Technology Co., Ltd. All rights reserved.<br/>
+ */
+class GlidePreLoad {
+}
